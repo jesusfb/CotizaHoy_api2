8eee62a0b61b0bd7fb7e6783a3ab37e022cfc4c2
jesusfb <jfigueroa.beltran@gmail.com>
2024-08-23 17:43:58 -0700

    Update CotizacionesController.cs

--- Controllers/CotizacionesController.cs
+++ Controllers/CotizacionesController.cs
@@ -1,6 +1,7 @@
 ﻿using CotizaHoyAPI.Services.Clientes;
 using CotizaHoyAPI.Services.Cotizaciones;
 using DotNet8WebAPI.Helpers;
+using DotNet8WebAPI.Model;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
