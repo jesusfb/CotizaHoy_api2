21a5a13cd63bcab61f0565a374909e79d1158f7f
jesusfb <jfigueroa.beltran@gmail.com>
2024-08-23 18:42:57 -0700

    Update Cotizaciones.cs

--- Model/Cotizaciones.cs
+++ Model/Cotizaciones.cs
@@ -11,25 +11,25 @@ public partial class Cotizaciones
     
     public int Id { get; set; }
 
-    [Required]
+
     public int ClienteFk { get; set; }
 
-    [Required]
+   
     public int ProductoFk { get; set; }
 
-    [Required]
+ 
     public double Cantidad { get; set; }
 
-    [Required]
+ 
     public decimal Precio { get; set; }
 
-    [Required]
+   
     public decimal CostoTotal { get; set; }
 
-    [Required]
+   
     public bool Iva { get; set; }
 
-    [Required]
+   
     public DateTime Fecha { get; set; }
 
 
